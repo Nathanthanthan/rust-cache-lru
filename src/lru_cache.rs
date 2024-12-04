202117b7e6f028e97fbb8d4c2a72354ed4b04fea
Nathanthanthan <nathan.petron@yahoo.com>
2024-12-04 09:17:58 +0100

    docs(LRU cache): :memo: Updated documentation

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -1,16 +1,18 @@
 use std::collections::HashMap;
 
-/// ### LRU cache implementation
-///
 /// A cache that can hold a choosen quantity of elements,
 /// deleting excess elements by order of least used.
+///
+/// # Type generics
+/// * `K` - Key type.
+/// * `V` - Value type.
 pub struct LRUCache<K, V> {
-    // The cache's maximum size
+    /// The cache's maximum size
     capacity: usize,
-    // HashMap containing all cached elements
+    /// HashMap containing all cached elements
     cached_elements: HashMap<K, V>,
-    // Vector containing all the keys of currently cached elements,
-    // from least to most recently used
+    /// Vector containing all the keys of currently cached elements,
+    /// from least to most recently used
     elements_order: Vec<K>,
 }
 
@@ -20,6 +22,9 @@ where
     V: std::fmt::Debug,
 {
     /// Creates a new instance of LRU cache with the provided capacity.
+    ///
+    /// **Attributes:**
+    /// * `capacity` - The cache's maximum size.
     pub fn new(capacity: usize) -> Self {
         LRUCache {
             capacity,
@@ -30,6 +35,9 @@ where
 
     /// Returns the element corresponding to the provided key
     /// and reorders said element as the MRU.
+    ///
+    /// **Attributes:**
+    /// * `key` - The key of the element to reorder and return.
     pub fn get(&mut self, key: K) -> Option<&V> {
         if self.cached_elements.contains_key(&key) {
             self.update_order(key);
@@ -45,8 +53,12 @@ where
 
     /// If the provided key already exists in the cache, update the corresponding element's value.
     /// Otherwise, add the provided key/value pair to the cache, removing the LRU excess element in the process.
-    /// 
+    ///
     /// In both cases, the provided element is reordered as the MRU.
+    ///
+    /// **Attributes:**
+    /// * `key` - The key of the element to update/insert.
+    /// * `value` - The value of the updated/inserted element.
     pub fn put(&mut self, key: K, value: V) {
         if self.cached_elements.contains_key(&key) {
             self.cached_elements.insert(key.clone(), value);
@@ -63,6 +75,9 @@ where
     }
 
     /// Reorders the provided key as the MRU.
+    /// 
+    /// **Attributes:**
+    /// * `key` - The key of the element to reorder as MRU.
     fn update_order(&mut self, key: K) {
         if let Some(pos) = self.elements_order.iter().position(|k| k == &key) {
             self.elements_order.remove(pos);
